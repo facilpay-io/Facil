0c854fc0608efe20719626774b06a20c927cabe4
Danny Nunez <dnunez22@gmail.com>
2024-01-12 15:37:24 -0500

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main className="bg-white flex min-h-screen flex-col items-center justify-between p-5">
   <div className="navbarHome flex justify-between items-center w-full">
     <div className="flex items-center">
-      <Image className="mr-24 ml-8 mt-1" src="/logo.png" alt="Description" width={81} height={64} />
+      <Image className="mr-24 ml-8 mt-1" src="/logov1.png" alt="Description" width={81} height={64} />
       <div className="flex space-x-14 text-white pt-1 ml-28 hidden lg:flex md:flex">
         <div className="butWrapHome absolute z-0 top-9 ml-4 "></div>
         <Link className="buttonHome z-10" href="/">Home</Link>
