b9f8bcd6bee8373bb7f4299b054567db2ac13af8
Danny Nunez <dnunez22@gmail.com>
2024-02-11 11:34:55 -0500

    updates

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const { parsed: localEnv } = require('dotenv').config();
+const webpack = require('webpack');
 
-module.exports = nextConfig
+const nextConfig = {
+  webpack(config) {
+    // Check if localEnv is defined
+    if (localEnv && typeof localEnv === 'object') {
+      // If localEnv is defined and it's an object, proceed with adding EnvironmentPlugin
+      config.plugins.push(new webpack.EnvironmentPlugin(localEnv));
+    } else {
+      // If localEnv is undefined or not an object, log an error
+      console.error('Error loading environment variables from .env file:', localEnv);
+    }
+    return config;
+  },
+};
+
+module.exports = nextConfig;
