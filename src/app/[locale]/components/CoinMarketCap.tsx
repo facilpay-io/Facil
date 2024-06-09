aacb64c8eda2e599791d015178914c5d5088b2c5
Danny Nunez <dnunez22@gmail.com>
2024-06-08 23:37:45 -0400

    new coin api

--- src/app/[locale]/components/CoinMarketCap.tsx
+++ src/app/[locale]/components/CoinMarketCap.tsx
@@ -8,47 +8,12 @@ const CoinMarketCap: React.FC = () => {
 
   useEffect(() => {
     const fetchData = async () => {
-      const cachedData = localStorage.getItem('coinMarketCapData');
-      const cachedTimestamp = localStorage.getItem('coinMarketCapTimestamp');
-
-      if (cachedData && cachedTimestamp) {
-        const parsedData = JSON.parse(cachedData);
-        const timestamp = parseInt(cachedTimestamp, 10);
-
-        // Check if cached data is not older than 5 minutes
-        if (Date.now() - timestamp < 5 * 60 * 1000) {
-          const totalMarketCapUSD = parsedData.quote.USD.total_market_cap;
-          const totalMarketCapYesterdayUSD = parsedData.quote.USD.total_market_cap_yesterday;
-          setTotalMarketCap(totalMarketCapUSD);
-          setTotalMarketCapYesterday(totalMarketCapYesterdayUSD);
-          
-          // Calculate percentage change
-          if (totalMarketCapYesterdayUSD) {
-            const difference = totalMarketCapUSD - totalMarketCapYesterdayUSD;
-            const percentage = (difference / totalMarketCapYesterdayUSD) * 100;
-            setPercentageChange(percentage);
-            // console.log('Percentage Change:', percentage);
-          }
-          
-          console.log('Using cached data');
-          return;
-        }
-      }
-
       try {
-        const response = await axios.get('https://corsproxy.io/?https://pro-api.coinmarketcap.com/v1/global-metrics/quotes/latest', {
-          headers: {
-            'X-CMC_PRO_API_KEY': process.env.NEXT_PUBLIC_CMC_API_KEY,
-          },
-        });
+        const response = await axios.get('https://raw.githubusercontent.com/Danny-Nunez/CoinMarketCap-Api/main/coinmarket.json');
 
         const json = response.data;
         const totalMarketCapUSD = json.data.quote.USD.total_market_cap;
         const totalMarketCapYesterdayUSD = json.data.quote.USD.total_market_cap_yesterday;
-
-        // Cache the data and timestamp
-        localStorage.setItem('coinMarketCapData', JSON.stringify(json.data));
-        localStorage.setItem('coinMarketCapTimestamp', Date.now().toString());
 
         setTotalMarketCap(totalMarketCapUSD);
         setTotalMarketCapYesterday(totalMarketCapYesterdayUSD);
@@ -62,23 +27,14 @@ const CoinMarketCap: React.FC = () => {
         }
 
         console.log('New data fetched:', totalMarketCapUSD);
-        // console.log('MC Yesterday fetched:', totalMarketCapYesterdayUSD);
-        // console.log(response.data);
+        console.log('MC Yesterday fetched:', totalMarketCapYesterdayUSD);
+        console.log(response.data);
       } catch (ex) {
         console.error('Error fetching data:', ex);
-
-        // Set a timer to refetch data after 1 minute
-        setTimeout(() => {
-          fetchData();
-        }, 5 * 60 * 1000);
       }
     };
 
     fetchData();
-
-    return () => {
-      // Cleanup function if needed
-    };
   }, []);
 
   return (
